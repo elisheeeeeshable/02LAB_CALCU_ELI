eeeb2735a61b2f71a5e820ca46eb1d8b43f5ea88
ivan <oncepidorobin34@gmail.com>
2025-08-27 14:09:01 +0800

    Add project files.

---
+++ 02LAB_CALCU_ELI/CalculatorClass.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02LAB_CALCU_ELI
+{
+    public delegate I Formula<I>(I arg1, I arg2);
+    class CalculatorClass
+    {
+        public Formula<double> formula;
+
+        public double GetSum(double a, double b)
+        {
+            return a + b;
+        }
+        public double GetDifference(double a, double b)
+        {
+            return a - b;
+        }
+        public double GetProduct(double a, double b)
+        {
+            return a * b;
+        }
+        public double GetQuotient(double a, double b)
+        {
+            if (b == 0)
+            {
+                return double.NaN; //NaN ibig sabihin Not a Number:)
+            }
+            return a / b;
+        }
+
+        private event Formula<double> calculateEvent;
+        public event Formula<double> CalculateEvent
+        {
+            add
+            {
+                Console.WriteLine("Added the Delegate");
+                calculateEvent += value;
+            }
+            remove
+            {
+                Console.WriteLine("Removed the Delegate");
+                calculateEvent -= value;
+            }
+        }
+    }
+}
+
+    
---
+++ 02LAB_CALCU_ELI/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _02LAB_CALCU_ELI
+{
+    public partial class Form1 : Form
+    {
+        CalculatorClass cal;
+        double num1, num2;
+        public Form1()
+        {
+            InitializeComponent();
+            cal = new CalculatorClass();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            num1 = Convert.ToDouble(textBox1.Text);
+            num2 = Convert.ToDouble(textBox2.Text);
+
+            string op = comboBox1.Text;
+
+            switch (op)
+            {
+                case "+":
+                    cal.CalculateEvent += new Formula<double>(cal.GetSum);
+                    lblTotal.Text = cal.GetSum(num1, num2).ToString();
+                    cal.CalculateEvent -= new Formula<double>(cal.GetSum);
+                    break;
+
+                case "-":
+                    cal.CalculateEvent += new Formula<double>(cal.GetDifference);
+                    lblTotal.Text = cal.GetDifference(num1, num2).ToString();
+                    cal.CalculateEvent -= new Formula<double>(cal.GetDifference);
+                    break;
+
+                case "*":
+                    cal.CalculateEvent += new Formula<double>(cal.GetProduct);
+                    lblTotal.Text = cal.GetProduct(num1, num2).ToString();
+                    cal.CalculateEvent -= new Formula<double>(cal.GetProduct);
+                    break;
+
+                case "/":
+                    cal.CalculateEvent += new Formula<double>(cal.GetQuotient);
+                    lblTotal.Text = cal.GetQuotient(num1, num2).ToString();
+                    cal.CalculateEvent -= new Formula<double>(cal.GetQuotient);
+                    break;
+            }
+        }
+    }
+}
